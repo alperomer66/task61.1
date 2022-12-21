e88fc411acf6ffbb6b5c7668a5df9d1fd0686fc2
Alper Yumer <112901293+alperomer66@users.noreply.github.com>
2022-12-21 22:54:04 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+let hotInHere = document.querySelectorAll('.hot');
+let text = hotInHere.textContent;
+hotInHere.forEach(element => {
+  if(!element.textContent.includes('🔥')){
+  element.textContent += '🔥'
+  }
+  });
 });
